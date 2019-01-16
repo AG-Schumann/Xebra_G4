c482a756f10090ecca046ef750ec3f2e2635336f
ab602 <ab602@vm-ltsp-deb9.physik.privat>
2019-01-16 10:58:56 +0100

    Prepared XebraConstructCryostat class.

--- include/XebraConstructCryostat.hh
+++ include/XebraConstructCryostat.hh
@@ -37,68 +37,29 @@ public:
    G4double GetOuterHeightCryostat();
    G4LogicalVolume* GetMotherVolume();
 
-   G4UnionSolid * BuildVesselShape( G4double Diameter_cyl, G4double Height_cyl, G4double Thickness_Wall );
-   G4SubtractionSolid * BuildFillerShape( G4double Diameter_cyl, G4double Height_cyl, G4double Thickness_Wall );
-
   void PrintGeometryInformation();
 
 private:
 
-  //Materials
-  G4Material *CryostatMaterial;
-  G4Material *Vacuum;
-  G4Material *LXe;
-  G4Material *GXe;
-
-
   //Parameters for the Cryostat Envelop
-  G4double CryostatEnvelop_Radius;  
-  G4double CryostatEnvelop_Height;
+  G4double Cryostat_Envelop_Radius;  
+  G4double Cryostat_Envelop_Height;
+
   //Parameters for the outer Cryostat
-  G4double OuterCryo_WallThickness; 
-  G4double OuterCryo_InnerCylDiameter; 
-  G4double OuterCryo_InnerCylHeight; 
-  G4double OuterCryo_FlangeHeight;
-  G4double OuterCryo_FlangeThickness;
-  G4double OuterCryo_StiffenerHeight;
-  G4double OuterCryo_StiffenerThickness;
-  int OuterCryo_StiffenerNumber;
+
+
   //Parameters for the inner Cryostat
-  G4double InnerCryo_WallThickness; 
-  G4double InnerCryo_InnerCylDiameter; 
-  G4double InnerCryo_InnerCylHeight; 
-  G4double InnerCryo_FlangeHeight;
-  G4double InnerCryo_FlangeThickness;
-  G4double InnerCryo_StiffenerHeight;
-  G4double InnerCryo_StiffenerThickness;
-  int InnerCryo_StiffenerNumber;
+
 
   //LogicalVolumes
-  G4LogicalVolume* CryostatVolume_Logical;
-  G4LogicalVolume* OuterCryostatVessel_Logical;
-  G4LogicalVolume* OuterCryostatVolume_Logical;
+  G4LogicalVolume* Cryostat_Envelop_log;
 
-  G4LogicalVolume* InnerCryostatVessel_Logical;
-  G4LogicalVolume* InnerCryostatVolume_Logical;
-  G4LogicalVolume* InnerCryostatBottomVolume_Logical;
-  G4LogicalVolume* InnerCryostatBottomFillerVessel_Logical;
-  G4LogicalVolume* InnerCryostatBottomFillerVolume_Logical;
-  G4LogicalVolume* InnerCryostatTPCVolume_Logical;
 
   //PhysicalVolumes
-  G4VPhysicalVolume* OuterCryostatVessel_Physical;
-  G4VPhysicalVolume* OuterCryostatVolume_Physical;
 
-  G4VPhysicalVolume* InnerCryostatVessel_Physical;
-  G4VPhysicalVolume* InnerCryostatVolume_Physical;
-  G4VPhysicalVolume* InnerCryostatBottomVolume_Physical;
-  G4VPhysicalVolume* InnerCryostatBottomFillerVessel_Physical;
-  G4VPhysicalVolume* InnerCryostatBottomFillerVolume_Physical;
-  G4VPhysicalVolume* InnerCryostatTPCVolume_Physical;
 
   //Visualization
   G4VisAttributes *VacuumVisAtt;
-  G4VisAttributes *TitaniumVisAtt;
   G4VisAttributes *GXeVisAtt;
   G4VisAttributes *LXeVisAtt;
 };
