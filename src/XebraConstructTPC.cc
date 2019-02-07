2f22234cac7150f5daa986b833e3c342a3a8924f
ab602 <ab602@vm-ltsp-deb9.physik.privat>
2019-02-07 17:07:09 +0100

    Stable version for calibration measurements, still have to fix PMTHitsCollection.

--- src/XebraConstructTPC.cc
+++ src/XebraConstructTPC.cc
@@ -776,7 +776,7 @@ using std::stringstream;
 	pSDManager->AddNewDetector(pLXeSD);
 
 	// ToDo: check which volumes to make LXe SD
-	/*
+/*
 	LXe_Logical->SetSensitiveDetector(pLXeSD);
 	LXe_extra_filling_log->SetSensitiveDetector(pLXeSD);
 	if (height_LXe_TPC_cylinder > 0.)	
@@ -787,7 +787,7 @@ using std::stringstream;
 		{
 		LXe_weir_2_log->SetSensitiveDetector(pLXeSD);
 		}
-	*/
+*/
 	LXe_ActiveVolume_Logical->SetSensitiveDetector(pLXeSD);
 	LXe_ActiveVolume_extra_filling_Logical->SetSensitiveDetector(pLXeSD);
 
